bcb03f05e8942763b478dcc9cf8e8185b6807d28
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2024-05-26 22:01:07 -0400

    Fundamental JavaScript syntax, control flow, basic functions

---
+++ notes/chapter_1.js
@@ -0,0 +1,15 @@
+let sentence = 'hello world'
+sentence = 'oops'
+
+let test = 1;
+if (test) {
+  console.log('happy')
+}
+
+let friends = {
+  james: 'is super cool',
+  lucy: 'isn\'t cool',
+  brainCellsShared: 0
+}
+
+let exampleArray = [0,1,2,3,4,5]
